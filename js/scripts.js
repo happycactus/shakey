125d95e4bfbac8c98737bb64ba19467e4f778048
Jermaine de Bruyne <jermaine.jamal@gmail.com>
2015-10-14 17:05:45 +0200

    orientation bug
    trying to check if it has passed the middle point before it does the
    opposite direction

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,65 @@
-(function($){
-	//GLOBAL VARIABLES
-	var game = new HCSegment('#segment');
-	game.preloadSettings();
+(function($) {
+    //GLOBAL VARIABLES
+    var game = new HCSegment('#segment');
+    var orientation = '';
+    var moveAllowed = false;
+    game.preloadSettings();
 
-	
-	$(document).ready(function(){
-		//DETECT orientation and create action
-		if (window.DeviceOrientationEvent) {
-			// Listen for the deviceorientation event and handle the raw data
-			//window.addEventListener('devicemotion', deviceMotionHandler);
-			window.addEventListener('deviceorientation', function(eventData) {
-				// gamma is the left-to-right tilt in degrees, where right is positive
-				var tiltLR = Math.round(eventData.gamma);
 
-				// beta is the front-to-back tilt in degrees, where front is positive
-				var tiltFB = Math.round(eventData.beta);
+    $(document).ready(function() {
+        //DETECT orientation and create action
+        if (window.DeviceOrientationEvent) {
+            // Listen for the deviceorientation event and handle the raw data
 
-				// alpha is the compass direction the device is facing in degrees
-				var dir = Math.round(eventData.alpha);
+            //window.addEventListener('devicemotion', deviceMotionHandler);
+            window.addEventListener('deviceorientation', function(eventData) {
+                // gamma is the left-to-right tilt in degrees, where right is positive
+                var tiltLR = Math.round(eventData.gamma);
 
-				$("#answer").text(tiltLR);// + " : " + tiltFB + " : " + dir);
-				
+                // beta is the front-to-back tilt in degrees, where front is positive
+                var tiltFB = Math.round(eventData.beta);
 
-				if (tiltLR < 0 || tiltFB < 0){
-					game.moveLeft(-tiltLR, 2);
-				} else {
-					game.moveRight(tiltLR, 2);
-					//$("#moRotation").text(tiltLR);
-				}
+                // alpha is the compass direction the device is facing in degrees
+                var dir = Math.round(eventData.alpha);
 
-			}, false);
-		} else {
-			$("#doEvent").text("Not supported.");
-		}
 
-		// setTimeout(function(){
-		// 	$('#tree').click();
-		// }, 3000);
-		// $('#tree').on('click', function(){
-		// 	game.shakeLeft(60, 300);
-		// });
-			
-            
+
+                if (tiltLR === 0) {
+                    orientation = 'center';
+                    moveAllowed = true;
+                }
+
+                if (tiltLR < 0) {
+                	orientation = 'left';
+
+                    game.moveLeft(-tiltLR, 2);
+
+                } else if (tiltLR > 0) {
+                	orientation = 'right';
+                    game.moveRight(tiltLR, 2);
+
+
+                    //$("#moRotation").text(tiltLR);
+                }
+
+                $("#answer").text(tiltLR + ': ' + orientation+' - '+moveAllowed); // + " : " + tiltFB + " : " + dir);
+
+            }, false);
+        } else {
+            $("#doEvent").text("Not supported.");
+        }
+
+        // setTimeout(function(){
+        // 	$('#tree').click();
+        // }, 3000);
+        // $('#tree').on('click', function(){
+        // 	game.shakeLeft(60, 300);
+        // });
+
+
         // function phoneShake() {
         //     //game.shakeLeft(70);
-        
+
         // }
 
         // $.shake({
@@ -55,18 +70,17 @@
 
 
 
-		//Allow only one click event
-		var inaction = false;
-		
-		//WINDOW RESIZE
-		$(window).resize(function(){
-		});
+        //Allow only one click event
+        var inaction = false;
 
-	});
-	
-	// $('a#knop').click(function(event){
-	//	event.preventDefault();
-		
-	// });
-	
+        //WINDOW RESIZE
+        $(window).resize(function() {});
+
+    });
+
+    // $('a#knop').click(function(event){
+    //	event.preventDefault();
+
+    // });
+
 })(jQuery);
